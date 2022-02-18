1ae01a75c20a8dbd5a0f08fc0cba2177fbeecd86
Alia <alivamallick9@gmail.com>
2022-02-18 11:08:55 +0530

    Vehicles type added

--- src/pages/Localization.js
+++ src/pages/Localization.js
@@ -1,5 +1,130 @@
+import MaterialTable from "@material-table/core";
+import { Avatar, Chip } from "@mui/material";
+import { ExportCsv, ExportPdf } from "@material-table/exporters";
+// import { BASE_URL } from "configs";
+import moment from "moment";
+
 const Localization = () => {
-  return <div>Localization</div>;
+  // const { days, setRealtime } = useDays();
+  // const handleBulkDelete = async (data) => {};
+  return (
+    <div style={{ marginTop: "2vh" }}>
+      <MaterialTable
+        options={{
+          selection: "true",
+          addRowPosition: "first",
+          actionsColumnIndex: -1,
+          pageSize: 10,
+          exportAllData: true,
+          exportMenu: [
+            {
+              label: "Export PDF",
+              exportFunc: (cols, datas) => ExportPdf(cols, datas, "Vehicles"),
+            },
+            {
+              label: "Export CSV",
+              exportFunc: (cols, datas) => ExportCsv(cols, datas, "Vehicles"),
+            },
+          ],
+        }}
+        title={"Vehicles"}
+        data={[
+          {
+            sl: 1,
+            typeImage: "",
+            typeName: "SUV",
+            costPerKm: 7,
+            seatingCapacity: 5,
+            status: "On",
+          },
+        ]}
+        columns={[
+          {
+            title: "#",
+            field: "sl",
+            editable: "never",
+            width: "10%",
+          },
+          {
+            title: "Type Image",
+            field: "typeImageUrl",
+            render: ({ typeImageUrl }) => (
+              <Avatar
+                variant="rounded"
+                sx={{ width: "12vh", height: "12vh" }}
+              />
+            ),
+            searchable: true,
+          },
+          {
+            title: "Name",
+            field: "typeName",
+            searchable: true,
+          },
+          {
+            title: "Cost Per Km",
+            field: "costPerKm",
+            type: "numeric",
+            searchable: true,
+          },
+          {
+            title: "Seating Capacity",
+            field: "seatingCapacity",
+            searchable: true,
+            type: "numeric",
+          },
+          {
+            title: "Status",
+            field: "status",
+            render: (row) => (
+              <>
+                <Chip
+                  size="small"
+                  variant="outlined"
+                  color="secondary"
+                  label={row?.status}
+                  sx={{ padding: "4px" }}
+                />
+              </>
+            ),
+          },
+          {
+            title: "Timestamp",
+            // width: "70%",
+            field: "timestamp",
+            editable: "never",
+            render: ({ timestamp }) => moment(timestamp).format("lll"),
+            export: false,
+            searchable: true,
+            // hidden: true,
+          },
+          {
+            title: "Timestamp",
+            // width: "70%",
+            field: "currentTimestamp",
+            editable: "never",
+            hidden: true,
+            export: true,
+            // render: ({ timestamp }) => moment(timestamp).format("lll"),
+          },
+        ]}
+        editable={{
+          onRowAdd: async (data) => {},
+          onRowUpdate: async (newData, oldData) => {},
+          onRowDelete: async (oldData) => {},
+        }}
+        // actions={[
+        //   {
+        //     tooltip: "Delete all selected Days",
+        //     icon: "delete",
+        //     onClick: (evt, data) =>
+        //       handleBulkDelete(data.map((data) => data?.day)),
+        //   },
+        // ]}
+        // isLoading={days === null}
+      />
+    </div>
+  );
 };
 
 export default Localization;
