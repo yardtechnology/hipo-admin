7b9b655be41eaf3e5c9878278a432b36dfb1970b
Alia <alivamallick9@gmail.com>
2022-03-28 13:17:18 +0530

    switch added

--- src/pages/AllDrivers.js
+++ src/pages/AllDrivers.js
@@ -13,10 +13,11 @@ import {
   Avatar,
   Card,
   CardContent,
-  Chip,
   ListItem,
   ListItemAvatar,
   ListItemText,
+  styled,
+  Switch,
   Tooltip,
   Typography,
 } from "@mui/material";
@@ -38,7 +39,60 @@ const AllDrivers = () => {
   const [openVehicleInfoDrawer, setOpenVehicleInfoDrawer] = useState(false);
   const [openDocumentDrawer, setOpenDocumentDrawer] = useState(false);
   const [openEditDriverDrawer, setOpenEditDriverDrawer] = useState(false);
-
+  const IOSSwitch = styled((props) => (
+    <Switch
+      focusVisibleClassName=".Mui-focusVisible"
+      disableRipple
+      {...props}
+    />
+  ))(({ theme }) => ({
+    width: 42,
+    height: 26,
+    padding: 0,
+    "& .MuiSwitch-switchBase": {
+      padding: 0,
+      margin: 2,
+      transitionDuration: "300ms",
+      "&.Mui-checked": {
+        transform: "translateX(16px)",
+        color: "#fff",
+        "& + .MuiSwitch-track": {
+          backgroundColor: theme.palette.mode === "dark" ? "#2ECA45" : "red",
+          opacity: 1,
+          border: 0,
+        },
+        "&.Mui-disabled + .MuiSwitch-track": {
+          opacity: 0.5,
+        },
+      },
+      "&.Mui-focusVisible .MuiSwitch-thumb": {
+        color: "#33cf4d",
+        border: "6px solid #fff",
+      },
+      "&.Mui-disabled .MuiSwitch-thumb": {
+        color:
+          theme.palette.mode === "light"
+            ? theme.palette.grey[100]
+            : theme.palette.grey[600],
+      },
+      "&.Mui-disabled + .MuiSwitch-track": {
+        opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
+      },
+    },
+    "& .MuiSwitch-thumb": {
+      boxSizing: "border-box",
+      width: 22,
+      height: 22,
+    },
+    "& .MuiSwitch-track": {
+      borderRadius: 26 / 2,
+      backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
+      opacity: 1,
+      transition: theme.transitions.create(["background-color"], {
+        duration: 500,
+      }),
+    },
+  }));
   return (
     <>
       {" "}
@@ -159,14 +213,27 @@ const AllDrivers = () => {
             field: "status",
             render: (row) => (
               <>
-                <Chip
+                {/* <Button
+                  sx={{ padding: "4px 5px", textTransform: "none" }}
                   size="small"
-                  variant="outlined"
-                  color="secondary"
-                  label={row?.status}
-                />
+                  variant="contained"
+                  color="success"
+                >
+                  {row?.status}
+                </Button> */}
+                <IOSSwitch size="small" checked={true} onChange={false} />
               </>
             ),
+            // render: (row) => (
+            //   <>
+            //     <Chip
+            //       size="small"
+            //       variant="outlined"
+            //       color="secondary"
+            //       label={row?.status}
+            //     />
+            //   </>
+            // ),
           },
           {
             title: "Actions",
@@ -293,16 +360,16 @@ const AllDrivers = () => {
             icon: "send",
             onClick: (evt, data) => setSelectedUsers(data),
           },
-          // {
-          //   tooltip: "Block all selected users",
-          //   icon: "block",
-          //   // onClick: (evt, data) => setSelectedUsers(data),
-          // },
-          // {
-          //   tooltip: "Unblock all selected users",
-          //   icon: "done",
-          //   // onClick: (evt, data) => setSelectedUsers(data),
-          // },
+          {
+            tooltip: "Block all selected users",
+            icon: "block",
+            // onClick: (evt, data) => setSelectedUsers(data),
+          },
+          {
+            tooltip: "Unblock all selected users",
+            icon: "done",
+            // onClick: (evt, data) => setSelectedUsers(data),
+          },
         ]}
         detailPanel={({ rowData }) => {
           return (
--- src/pages/Riders.js
+++ src/pages/Riders.js
@@ -3,10 +3,11 @@ import { ExportCsv, ExportPdf } from "@material-table/exporters";
 import { History, LocationCity, PersonAdd } from "@mui/icons-material";
 import {
   Avatar,
-  Button,
   ListItem,
   ListItemAvatar,
   ListItemText,
+  styled,
+  Switch,
   Tooltip,
   Typography,
 } from "@mui/material";
@@ -15,6 +16,56 @@ import { SendNotification } from "components/dialog";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import moment from "moment";
+const IOSSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  "& .MuiSwitch-switchBase": {
+    padding: 0,
+    margin: 2,
+    transitionDuration: "300ms",
+    "&.Mui-checked": {
+      transform: "translateX(16px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        backgroundColor: theme.palette.mode === "dark" ? "#2ECA45" : "red",
+        opacity: 1,
+        border: 0,
+      },
+      "&.Mui-disabled + .MuiSwitch-track": {
+        opacity: 0.5,
+      },
+    },
+    "&.Mui-focusVisible .MuiSwitch-thumb": {
+      color: "#33cf4d",
+      border: "6px solid #fff",
+    },
+    "&.Mui-disabled .MuiSwitch-thumb": {
+      color:
+        theme.palette.mode === "light"
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    "&.Mui-disabled + .MuiSwitch-track": {
+      opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxSizing: "border-box",
+    width: 22,
+    height: 22,
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
+    opacity: 1,
+    transition: theme.transitions.create(["background-color"], {
+      duration: 500,
+    }),
+  },
+}));
 const Riders = () => {
   const [selectedUsers, setSelectedUsers] = useState([]);
   const [openAddressDrawer, setOpenAddressDrawer] = useState(false);
@@ -138,14 +189,15 @@ const Riders = () => {
             width: "5%",
             render: (row) => (
               <>
-                <Button
+                {/* <Button
                   sx={{ padding: "4px 5px", textTransform: "none" }}
                   size="small"
                   variant="contained"
                   color="success"
                 >
                   {row?.status}
-                </Button>
+                </Button> */}
+                <IOSSwitch size="small" checked={true} onChange={false} />
               </>
             ),
           },
