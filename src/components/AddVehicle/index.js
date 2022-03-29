95f44cff030acd00944beffbc2db824038bf7ae6
Alia <alivamallick9@gmail.com>
2022-03-29 11:50:38 +0530

    edit vehicle drawer schema

--- src/components/AddVehicle/index.js
+++ src/components/AddVehicle/index.js
@@ -3,3 +3,4 @@ export { default as RCInfo } from "./RCInfo";
 export { default as InsuranceInfo } from "./InsuranceInfo";
 export { default as EditRC } from "./EditRC";
 export { default as EditInsurance } from "./EditInsurance";
+export { default as EditVehicle } from "./EditVehicle";
