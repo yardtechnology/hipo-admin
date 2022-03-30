8b2c1271d7936e1a81c7be9fcc5d876349180435
Alia <alivamallick9@gmail.com>
2022-03-30 13:35:33 +0530

    edit qa drawer

--- src/schemas/QASchema.jsx
+++ src/schemas/QASchema.jsx
@@ -4,23 +4,21 @@ const QASchema = [
   {
     key: "1",
     label: "Question",
-    name: "question",
-    validationSchema: Yup.string()
-      .required("Question is Required")
-      .max(50, "Question must be less than 50 characters")
-      .min(5, "Question must be greater than 5 characters"),
+    name: "questions",
+    validationSchema: Yup.string().required("Question is Required"),
+    //   .max(50, "Question must be less than 50 characters")
+    //   .min(5, "Question must be greater than 5 characters"),
     initialValue: "",
   },
   {
     key: "2",
     label: "Answer",
-    name: "answer",
+    name: "answers",
     multiline: true,
-    rows: 4,
-    validationSchema: Yup.string()
-      .required("Answer is Required")
-      .max(350, "Answer must be less than 350 characters")
-      .min(5, "Answer must be greater than 5 characters"),
+    rows: 6,
+    validationSchema: Yup.string().required("Answer is Required"),
+    //   .max(350, "Answer must be less than 350 characters")
+    //   .min(5, "Answer must be greater than 5 characters"),
     initialValue: "",
   },
 ];
