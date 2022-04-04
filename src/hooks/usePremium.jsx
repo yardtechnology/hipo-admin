5a1fc06be75e88413cdc39d2f3dc65ec47da8003
Alia <alivamallick9@gmail.com>
2022-04-04 16:28:28 +0530

    premium hook

--- src/hooks/usePremium.jsx
+++ src/hooks/usePremium.jsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+import { useIsMounted } from "hooks";
+import { BASE_URL } from "configs";
+
 const usePremium = () => {
-  return <div>usePremium</div>;
+  const [premium, setPremium] = useState(null);
+  const [realtime, setRealtime] = useState(false);
+  const { isMounted } = useIsMounted();
+  useEffect(() => {
+    const fetchPremiums = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/premiums/all`, {
+          method: "GET",
+          // body: JSON.stringify({ ...values }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("SAL")}`,
+          },
+        });
+        const arr = await response.json();
+        console.log(arr);
+        const sortArr = arr?.data?.sort(
+          (a, b) => new Date(b?.createdAt) - new Date(a?.createdAt)
+        );
+        isMounted.current && setPremium(sortArr);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPremiums();
+  }, [isMounted, realtime]);
+  return {
+    premium,
+    setRealtime,
+  };
 };
 
 export default usePremium;
