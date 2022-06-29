c6ec314f7010cea8532ecae56e759227c030f891
Alia <alivamallick9@gmail.com>
2022-06-29 15:27:21 +0530

    minor changes

--- src/hooks/useActiveRides.jsx
+++ src/hooks/useActiveRides.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useIsMounted } from "hooks";
 import { BASE_URL } from "configs";
 
@@ -6,28 +6,28 @@ const useActiveRides = () => {
   const [activeRides, setActiveRides] = useState(null);
   const { isMounted } = useIsMounted();
   const [realtime, setRealtime] = useState(false);
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/rides/all?status=ACTIVE`, {
+        // method: "GET",
+        // body: JSON.stringify({ ...values }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("SAL")}`,
+        },
+      });
+      const arr = await response.json();
+      const sortArr = arr?.data?.sort(
+        (a, b) => new Date(b?.createdAt) - new Date(a?.createdAt)
+      );
+      isMounted.current && setActiveRides(sortArr);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [isMounted]);
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${BASE_URL}/rides/all?status=ACTIVE`, {
-          // method: "GET",
-          // body: JSON.stringify({ ...values }),
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("SAL")}`,
-          },
-        });
-        const arr = await response.json();
-        const sortArr = arr?.data?.sort(
-          (a, b) => new Date(b?.createdAt) - new Date(a?.createdAt)
-        );
-        isMounted.current && setActiveRides(sortArr);
-      } catch (error) {
-        console.log(error);
-      }
-    };
     fetchData();
-  }, [isMounted, realtime]);
+  }, [isMounted, realtime, fetchData]);
   return {
     activeRides,
     setRealtime,
