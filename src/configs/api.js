70894aa4c4e4586e8d3dd3732432977c61627782
ALIA <alivamallick9@gmail.com>
2023-05-23 12:02:44 +0530

    base url updated

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,3 +1,4 @@
-const BASE_URL = "https://hipo-dev-api.herokuapp.com/api";
+const BASE_URL = "https://hipo-dev-api.onrender.com/api";
+// const BASE_URL = "http://192.168.213.26:8000/api";
 const MAP_KEY = "AIzaSyBFteIJ7PGw3Tc0iGxs56rpveHYTPX9t_c";
 export { BASE_URL, MAP_KEY };
